b651c88c71ec775c773ed0a10a0217016f833d87
OmerOzdil <omerozdil54@gmail.com>
2022-04-23 15:23:08 +0100

    conflict

--- src/test/java/com/vytrack/pages/DashboardPage.java
+++ src/test/java/com/vytrack/pages/DashboardPage.java
@@ -3,4 +3,5 @@ package com.vytrack.pages;
 public class DashboardPage extends BasePage {
     // here is the new method created
     //I am adding new method here
+    // We are creating an conflict delibaretely
 }
